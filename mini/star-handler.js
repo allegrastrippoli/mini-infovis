df543977c3782f10346fa3f2ac0521e868fc2404
Allegra Strippoli <allegra@strippoli.eu>
2024-09-14 18:41:45 +0200

    fix: rm main func

--- mini/star-handler.js
+++ mini/star-handler.js
@@ -1,15 +1,34 @@
-async function main() {
+const color = d3.scaleOrdinal(d3.schemeCategory10);
+const spikes = 5;
+const outerRadius = 20;
+const innerRadius = 10;
+const width = window.innerWidth;
+const height = window.innerHeight;
 
-    const coordinates = await fetch('stars.json')
-    .then(response => response.json());
+function starPoints(cx, cy, spikes, outerRadius, innerRadius) {
+    let rot = Math.PI / 2 * 3;
+    let x = cx;
+    let y = cy;
+    let step = Math.PI / spikes;
+    let path = "";
 
-    const color = d3.scaleOrdinal(d3.schemeCategory10);
-    const spikes = 5
-    const outerRadius = 20
-    const innerRadius = 10
-    const width = window.innerWidth;
-    const height = window.innerHeight;
+    for (let i = 0; i < spikes; i++) {
+        x = cx + Math.cos(rot) * outerRadius;
+        y = cy - Math.sin(rot) * outerRadius;
+        path += `${x},${y} `;
+        rot += step;
 
+        x = cx + Math.cos(rot) * innerRadius;
+        y = cy - Math.sin(rot) * innerRadius;
+        path += `${x},${y} `;
+        rot += step;
+    }
+
+    return path;
+}
+
+
+d3.json('stars.json').then(coordinates => {
     const x_max = Math.max(
         ...coordinates.map(coord => Math.max(coord.x1, coord.x2, coord.x3))
     );
@@ -18,30 +37,7 @@ async function main() {
     );
 
     const xScale = d3.scaleLinear().domain([0, x_max]).range([2 * outerRadius, width - 2 * outerRadius]);
-    
     const yScale = d3.scaleLinear().domain([0, y_max]).range([2 * outerRadius, height - 2 * outerRadius]);
-
-    function starPoints(cx, cy, spikes, outerRadius, innerRadius) {
-        let rot = Math.PI / 2 * 3;
-        let x = cx;
-        let y = cy;
-        let step = Math.PI / spikes;
-        let path = "";
-
-        for (let i = 0; i < spikes; i++) {
-            x = cx + Math.cos(rot) * outerRadius;
-            y = cy - Math.sin(rot) * outerRadius;
-            path += `${x},${y} `;
-            rot += step;
-
-            x = cx + Math.cos(rot) * innerRadius;
-            y = cy - Math.sin(rot) * innerRadius;
-            path += `${x},${y} `;
-            rot += step;
-        }
-
-        return path;
-    }
 
     const svgContainer = d3.select("svg")
         .attr("width", width)
@@ -81,5 +77,4 @@ async function main() {
                 .attr("points", starPoints(xScale(nextx), yScale(nexty), spikes, outerRadius, innerRadius));
         });
     });
-}
-
+});
